ef0c2a23887baf9f1ca1e6d9bed19e8009c3dd9a
Yuliannys <CarvajalYJC@gmail.com>
2018-05-19 00:01:46 -0400

    Agregados los comentarios al codigo en JS

--- app.js
+++ app.js
@@ -2,14 +2,7 @@ var buttomLogin = document.getElementById('loginName');
 
 buttomLogin.addEventListener('click', function () {
 
-   var nameLoad = null;
-   do {
-       nameLoad = ('¿Cual es tu nombre?');
-   }
-   while (false == nameLoad || null === nameLoad);
-
- // agregar instrucciones
-
+ // Instrucciones
    document.getElementById('loginName').style.display = 'none';
    document.getElementById('userName').innerHTML = 'Bienvenid@ ' + nameLoad;
    document.getElementById('queries').style.visibility = 'visible';
@@ -17,46 +10,36 @@ buttomLogin.addEventListener('click', function () {
 });
 
 function reply() {
-
+// Pregunta uno.
    var questionOne = '';
-
    if (document.getElementById('answerOne').checked) {
        questionOne = document.getElementById('answerOne').value;
    }
-
    if (document.getElementById('answerTwo').checked) {
        questionOne = document.getElementById('answerTwo').value;
    }
-
    if (document.getElementById('answerThree').checked) {
        questionOne = document.getElementById('answerThree').value;
    }
-
-
+// Pregunta dos.
    var questionTwo = '';
-
    if (document.getElementById('answerFour').checked) {
        questionTwo = document.getElementById('answerFour').value;
    }
-
    if (document.getElementById('answerFive').checked) {
        questionTwo = document.getElementById('answerFive').value;
    }
-
    if (document.getElementById('answerSix').checked) {
        questionTwo = document.getElementById('answerSix').value;
    }
-
+// Pregunta tres.
    var questionThree = '';
-
    if (document.getElementById('answerSeven').checked) {
        questionThree = document.getElementById('answerSeven').value;
    }
-
    if (document.getElementById('answerEight').checked) {
        questionThree = document.getElementById('answerEight').value;
    }
-
    if (document.getElementById('answerNine').checked) {
        questionThree = document.getElementById('answerNine').value;
    }
